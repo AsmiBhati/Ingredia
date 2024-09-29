5b1640a71aa56d2e6178c2d9227ba01d80adc662
AsmiBhati <asmi.bhati9@gmail.com>
2024-09-30 01:21:27 +0530

    form linked to landing page

--- src/main/java/com/example/ingredia/contoller/IngrediaController.java
+++ src/main/java/com/example/ingredia/contoller/IngrediaController.java
@@ -1,17 +1,22 @@
 package com.example.ingredia.contoller;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IngrediaController {
 
+    @GetMapping("/form")
+    public String showForm() {
+        return "from";  // Make sure to return the name of the HTML file without .html
+    }
+
     @PostMapping("/submit")
     public String handleForm(@RequestParam("ingredient") String ingredient,
                              @RequestParam("calories") int calories) {
         // Process the form data (ingredient and calories)
         System.out.println("Ingredient: " + ingredient + ", Calories: " + calories);
-        return "redirect:/src/main/resources/static/from.html";  // Redirect back to the form page or another page
+        return "redirect:/form";  // Redirect back to the form page or another page
     }
 }
